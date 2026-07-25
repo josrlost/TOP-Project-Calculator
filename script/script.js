25e9f478e848165e3d1cafeda16869a261856c2c
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-25 18:02:40 +0000

    Created the basic HTML index file, with the basic html structure for the calculator interface, including a main div container for the calculator, a span container for the span, button elements for the basic operators, and a child div container for the number pad that parents the buttons for each number in a calculator. Also created a basic script.js file linked to the html file, that defines basic functions for the basic math operations, two variables for the number, a variable for the operator, and a function to operate on those two number variables by calling a operating function

---
+++ script/script.js
@@ -0,0 +1,28 @@
+// Basic math operations functions:
+function adding(num1, num2) {
+    num1 + num2;
+}
+
+function substracting(num1, num2) {
+    num1 - num2;
+}
+
+function multiplying(num1, num2) {
+    num1 * num2;
+}
+
+function dividing(num1, num2) {
+    num1 / num2;
+}
+
+//Variables for each part of the basic math operation
+let firstNumber = 0;
+let operator;
+let nextNumber = 0;
+
+//New function 'operate':
+function operate(operator, num1, num2) {
+    adding(num1, num2);
+}
+
+//Functions that update number variables with btn pressin:
