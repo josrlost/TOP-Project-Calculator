16851dcd541b6621c45df4670122f9ba683faeda
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-25 20:26:16 +0000

    Reference each DOM node on its respective js variable. Created a variable for each numeric and operator buttons. Also assign the nodelist with its corresponding class selector of numericBtn to a generic variable so to use the forEach method to iterate over every numericBtn using a single method. Within the forEach method loop added an click even listener with a change number function. The change number function has a switch statement that logs the respective number based on what button is pressed within the buttons nodelist

--- script/script.js
+++ script/script.js
@@ -15,14 +15,77 @@ function dividing(num1, num2) {
     num1 / num2;
 }
 
+
 //Variables for each part of the basic math operation
 let firstNumber = 0;
 let operator;
 let nextNumber = 0;
+
 
 //New function 'operate':
 function operate(operator, num1, num2) {
     adding(num1, num2);
 }
 
+
+//Variables for basic DOM nodes
+const calculator = document.querySelector("#calculator");
+const calcDisplay = document.querySelector("#calcDisplay");
+const plusBtn = document.querySelector("#plusSign");
+const minusBtn = document.querySelector("#minusSign");
+const multiplicationBtn = document.querySelector("#multiplicationSign");
+const divisionBtn = document.querySelector("#divisionSign");
+const equalBtn = document.querySelector("#equalSign");
+const clearBtn = document.querySelector("#clearBtn");
+const btnZero = document.querySelector("#btnZero");
+const btnOne = document.querySelector("#btnOne");
+const btnTwo = document.querySelector("#btnTwo");
+const btnThree = document.querySelector("#btnThree");
+const btnFour = document.querySelector("#btnFour");
+const btnFive = document.querySelector("#btnFive");
+const btnSix = document.querySelector("#btnSix");
+const btnSeven = document.querySelector("#btnSeven");
+const btnEight = document.querySelector("#btnEight");
+const btnNine = document.querySelector("#btnNine");
+const numericBtns = document.querySelectorAll(".numericBtn");
+
+
+
 //Functions that update number variables with btn pressin:
+function changeNum(evt) {
+    switch (evt.target) {
+        case btnZero:
+            console.log(num = 0);
+            break;
+        case btnOne:
+            console.log(num = 1);
+            break;
+        case btnTwo: 
+            console.log(num = 2);
+            break;
+        case btnThree:
+            console.log(num = 3);
+            break;
+        case btnFour:
+            console.log(num = 4);
+            break;
+        case btnFive:
+            console.log(num = 5);
+            break;
+        case btnSix:
+            console.log(num = 6);
+            break;
+        case btnSeven:
+            console.log(num = 7);
+            break;
+        case btnEight:
+            console.log(num = 8);
+            break;
+        case btnNine:
+            console.log(num = 9);
+            break;
+    };
+}
+numericBtns.forEach((button) => {
+    button.addEventListener("click", changeNum)
+})
