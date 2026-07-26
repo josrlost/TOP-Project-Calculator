07f7999c1732ccd34fe227468a52bb8c7eb63705
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-26 01:28:01 +0000

    Added functionality to the operator buttons so that the display screen reflects the respective onclick event. Still trying to figure out how to store two numeric values sequentially in two different variables whenever two numeric buttons are pressed

--- script/script.js
+++ script/script.js
@@ -35,6 +35,8 @@ const plusBtn = document.querySelector("#plusSign");
 const minusBtn = document.querySelector("#minusSign");
 const multiplicationBtn = document.querySelector("#multiplicationSign");
 const divisionBtn = document.querySelector("#divisionSign");
+const operatorBtns = document.querySelectorAll(".operator")
+console.log(operatorBtns);
 const equalBtn = document.querySelector("#equalSign");
 const clearBtn = document.querySelector("#clearBtn");
 const btnZero = document.querySelector("#btnZero");
@@ -48,54 +50,147 @@ const btnSeven = document.querySelector("#btnSeven");
 const btnEight = document.querySelector("#btnEight");
 const btnNine = document.querySelector("#btnNine");
 const numericBtns = document.querySelectorAll(".numericBtn");
+const arrOfNumBtns1 = Array.from(numericBtns);
 
 
 
 //Functions that update number variables with btn pressin:
-function changeNum(evt) {
+function changeNum1(evt) {
     switch (evt.target) {
         case btnZero:
             firstNumber = 0;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnOne:
             firstNumber = 1;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnTwo: 
             firstNumber = 2;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnThree:
             firstNumber = 3;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnFour:
             firstNumber = 4;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnFive:
             firstNumber = 5;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnSix:
             firstNumber = 6;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnSeven:
             firstNumber = 7;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnEight:
             firstNumber = 8;
-            calcDisplay.textContent = firstNumber
+            calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
         case btnNine:
             firstNumber = 9;
             calcDisplay.textContent = firstNumber;
+            console.log(firstNumber)
             break;
-    };
+    }; 
 }
+
+function changeNum2(evt) {
+    switch (evt.target) {
+        case btnZero:
+            nextNumber = 0;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnOne:
+            nextNumber = 1;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnTwo: 
+            nextNumber = 2;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnThree:
+            nextNumber = 3;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnFour:
+            nextNumber = 4;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnFive:
+            nextNumber = 5;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnSix:
+            nextNumber = 6;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnSeven:
+            nextNumber = 7;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnEight:
+            nextNumber = 8;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+        case btnNine:
+            nextNumber = 9;
+            calcDisplay.textContent = nextNumber;
+            console.log(nextNumber);
+            break;
+    }; 
+}
+
 numericBtns.forEach((button) => {
-    button.addEventListener("click", changeNum)
-})
+    button.addEventListener("click", changeNum1);
+    button.addEventListener("click", changeNum2);
+});
+
+function inputtingOperator(evt) {
+     switch (evt.target) {
+        case plusBtn:
+            operator = '+';
+            calcDisplay.textContent = operator;
+            break;
+        case minusBtn:
+            operator = '-';
+            calcDisplay.textContent = operator;
+            break;
+        case multiplicationBtn: 
+            operator = 'x';
+            calcDisplay.textContent = operator;
+            break;
+        case divisionBtn:
+            operator = '/';
+            calcDisplay.textContent = operator;
+            break;
+    }; 
+}
+
+operatorBtns.forEach((button) => {
+    button.addEventListener("click", inputtingOperator);
+});
